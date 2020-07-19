ff3688d272a48d90a6d2a1e71d2165335f8201b8
shaobin675 <shaobin4724@gmail.com>
2020-07-19 15:58:10 -0400

    initial

---
+++ article/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Article
+
+
+# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'author', 'is_deleted', 'create_time', 'last_updated_time')
+    ordering = ('id', )
+
+
+#admin.site.register(Article, ArticleAdmin)
